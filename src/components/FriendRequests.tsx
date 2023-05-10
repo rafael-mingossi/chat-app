a4f9150f80c616e8a7ae28f6c48b0faaa90db7b2
rafael-mingossi <rafaelmingossi@gmail.com>
2023-05-10 17:17:41 +1000

    friends request accept and deny

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -20,7 +20,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   );
 
   const acceptFriend = async (senderId: string) => {
-    await axios.post("/api/requests/accept", { id: senderId });
+    await axios.post("/api/friends/accept", { id: senderId });
 
     setFriendRequests((prev) =>
       prev.filter((request) => request.senderId !== senderId)
@@ -30,7 +30,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   };
 
   const denyFriend = async (senderId: string) => {
-    await axios.post("/api/requests/deny", { id: senderId });
+    await axios.post("/api/friends/deny", { id: senderId });
 
     setFriendRequests((prev) =>
       prev.filter((request) => request.senderId !== senderId)
@@ -49,6 +49,7 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             <UserPlus className={"text-black"} />
             <p className="font-medium text-lg">{request.senderEmail}</p>
             <button
+              onClick={() => acceptFriend(request.senderId)}
               aria-label="accept request"
               className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
@@ -56,10 +57,11 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             </button>
 
             <button
+              onClick={() => denyFriend(request.senderId)}
               aria-label="deny request"
-              className="w-8 h-8 bg-indigo-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
+              className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
-              <X className="font-semibold text-white w-3/4 h-3/4" />
+              <X className="font-semibold text-white  w-3/4 h-3/4" />
             </button>
           </div>
         ))
